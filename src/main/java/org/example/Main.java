ad0d9b85f2066320302f5eac7823905d7aa51789
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-12-04 21:56:12 +0900

    feat: channel을 통한 파일 읽기

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class Main {
+    public static void main(String[] args) {
+        Path path = Paths.get("/Users/nyong/Desktop/com.googlecode.iterm2.plist");
+
+        // 채널 객체를 파일 읽기 모드로 생성
+        try (FileChannel channel = FileChannel.open(path, StandardOpenOption.READ)) {
+            // 1024 바이트 크기를 가진 Buffer 생성
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            channel.read(buffer);
+
+            buffer.flip();
+            Charset charset = Charset.defaultCharset();
+            String inputData = charset.decode(buffer).toString();
+            System.out.println("inputData = " + inputData);
+
+            buffer.clear();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("파일 읽기 작업 실패");
+        }
+    }
+}
