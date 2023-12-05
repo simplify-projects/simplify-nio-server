e0c4433e3be424449999496ce8f113ac0b0cddd9
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-12-05 15:02:31 +0900

    reactor: NioServer

--- src/main/java/org/nio/NioServer.java
+++ src/main/java/org/nio/NioServer.java
@@ -1,5 +1,7 @@
 package org.nio;
 
+import org.socket.Server;
+
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
@@ -7,95 +9,97 @@ import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class NioServer {
+
+    private static final Boolean NON_BLOCKING = false;
+
     public static void main(String[] args) {
         try {
-            // Selector 생성
             Selector selector = Selector.open();
-
-            // ServerSocketChannel 생성 및 설정
             ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
             serverSocketChannel.bind(new InetSocketAddress(8080));
-            // Non-blocking 모드로 설정
-            serverSocketChannel.configureBlocking(false);
-
-            // Selector에 ServerSocketChannel 등록
+            serverSocketChannel.configureBlocking(NON_BLOCKING);
             serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
 
             while (true) {
-                // 이벤트 발생을 대기
+                // 이벤트 발생 대기
                 int readyChannels = selector.select();
+                // 만약 이벤트가 발생하지 않았다면 계속 대기
                 if (readyChannels == 0) {
                     continue;
                 }
-
-                // 발생한 이벤트 처리
-                Set<SelectionKey> selectedKeys = selector.selectedKeys();
-                Iterator<SelectionKey> keyIterator = selectedKeys.iterator();
-
-                while (keyIterator.hasNext()) {
-                    SelectionKey key = keyIterator.next();
-
-                    if (key.isAcceptable()) {
-                        // Accept 이벤트 처리
-                        ServerSocketChannel serverChannel = (ServerSocketChannel) key.channel();
-                        SocketChannel clientChannel = serverChannel.accept();
-                        clientChannel.configureBlocking(false);
-                        clientChannel.register(selector, SelectionKey.OP_READ);
-                    } else if (key.isReadable()) {
-                        // Read 이벤트 처리
-                        SocketChannel channel = (SocketChannel) key.channel();
-                        ByteBuffer buffer = ByteBuffer.allocate(1024);
-                        int bytesRead = channel.read(buffer);
-                        
-                        if (bytesRead == -1) {
-                            // 클라이언트가 연결을 종료한 경우
-                            key.cancel();
-                            channel.close();
-                        } else if (bytesRead > 0) {
-                            // 데이터를 받아온 경우
-                            buffer.flip();
-                            byte[] data = new byte[buffer.limit()];
-                            buffer.get(data);
-                            processReceivedData(data);
-
-                            // 다시 Selector에 쓰기 이벤트 등록
-                            key.interestOps(SelectionKey.OP_WRITE);
-                        }
-                    } else if (key.isWritable()) {
-                        // Write 이벤트 처리
-                        SocketChannel channel = (SocketChannel) key.channel();
-
-                        // TODO: 응답 데이터 작성
-                        byte[] responseData = generateResponseData();
-
-                        ByteBuffer buffer = ByteBuffer.wrap(responseData);
-                        channel.write(buffer);
-
-                        // 다시 Selector에 읽기 이벤트 등록
-                        key.interestOps(SelectionKey.OP_READ);
-                    }
-
-                    keyIterator.remove();
-                }
+                isExistEvents(selector);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    private static void isExistEvents(Selector selector) throws IOException {
+        // 발생한 이벤트 처리
+        Set<SelectionKey> selectedKeys = selector.selectedKeys();
+        Iterator<SelectionKey> keys = selectedKeys.iterator();
+
+        while (keys.hasNext()) {
+            SelectionKey key = keys.next();
+            if (key.isAcceptable()) {
+                acceptable(selector, key);
+            } else if (key.isReadable()) {
+                readable(key);
+            } else if (key.isWritable()) {
+                writeable(key);
+            }
+            keys.remove();
+        }
+    }
+
+    private static void acceptable(Selector selector, SelectionKey key) throws IOException {
+        ServerSocketChannel serverChannel = (ServerSocketChannel) key.channel();
+        SocketChannel clientChannel = serverChannel.accept();
+        clientChannel.configureBlocking(false);
+        clientChannel.register(selector, SelectionKey.OP_READ);
+    }
+
+    private static void readable(SelectionKey key) throws IOException {
+        SocketChannel channel = (SocketChannel) key.channel();
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        int bytesRead = channel.read(buffer);
+
+        // 클라이언트가 연결을 종료했다면
+        if (bytesRead == -1) {
+            key.cancel();
+            channel.close();
+        } else if (bytesRead > 0) {
+            buffer.flip();
+            byte[] data = new byte[buffer.limit()];
+            buffer.get(data);
+            processReceivedData(data);
+            key.interestOps(SelectionKey.OP_WRITE);
+        }
+    }
+
+    private static void writeable(SelectionKey key) {
+        SocketChannel channel = (SocketChannel) key.channel();
+        byte[] responseData = new byte[2048];
+        ByteBuffer buffer = ByteBuffer.wrap(responseData);
+        try {
+            channel.write(buffer);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        key.interestOps(SelectionKey.OP_READ);
+    }
+
     private static void processReceivedData(byte[] data) {
-        // TODO: 받은 데이터에 대한 비즈니스 로직 수행
-        // ...
+        String inputMessage = new String(data, StandardCharsets.UTF_8);
+        Logger logger = Logger.getLogger("NIO-Server");
+        logger.log(Level.INFO, inputMessage);
     }
 
-    private static byte[] generateResponseData() {
-        // TODO: 응답 데이터 생성
-        // ...
-
-        return new byte[0];
-    }
 }
